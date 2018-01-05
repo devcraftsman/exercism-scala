dfe3764e45f9df4406ad10bc119f1bedb535975b
devcraftsman <alessandro.brambill@gmail.com>
2018-01-05 15:53:30 +0100

    removed unused function

--- strain/src/main/scala/Strain.scala
+++ strain/src/main/scala/Strain.scala
@@ -1,15 +1,11 @@
 object Strain {
 
   def keep[T](list: Traversable[T], f: T => Boolean): List[T] = {
-    (splitOnCondition(list, f) _1) toList
+    (list partition f _1) toList
   }
 
   def discard[T](list: Traversable[T], f: T => Boolean): List[T] = {
-    (splitOnCondition(list, f) _2) toList
-  }
-
-  private def splitOnCondition[T](list: Traversable[T], f: T => Boolean) = {
-    list partition f
+    (list partition f _2) toList
   }
 
 
