5e347dea2919c123abc4a9ebd320e2ffbe163c15
devcraftsman <alessandro.brambill@gmail.com>
2018-01-12 10:02:37 +0100

    Simpler total method

--- grains/src/main/scala/Grains.scala
+++ grains/src/main/scala/Grains.scala
@@ -1,6 +1,6 @@
 object Grains {
 
-  def total: BigInt = (1 to 64).map(i => square(i)).foldLeft(BigInt(0))((acc, b) => b.map(_ + acc).getOrElse(0))
+  def total: BigInt = (1 to 64).flatMap(square).sum
 
   def square(i: Int): Option[BigInt] = {
     if (i <= 0 || i > 64) None
