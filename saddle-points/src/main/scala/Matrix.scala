f8f43509604ea1e0bffd635fb3a25045f8c75924
Brambilla <Alessandro.Brambilla@acer.com>
2019-05-15 11:55:32 +0200

    Saddle point

---
+++ saddle-points/src/main/scala/Matrix.scala
@@ -0,0 +1,28 @@
+
+case class Matrix(data : List[List[Int]]) {
+
+    type IndexedRow = (List[Int], Int)
+    type Element = ((Int, Int),Int)
+
+    def saddlePoints() : Set[(Int, Int)] =  {
+        def elmInRow(row : IndexedRow, c: (Int,Int) => Boolean) : List[Element] = row._1.zipWithIndex.foldLeft(List[Element]())(
+            (acc,elm) => {
+                if (!(acc.filter((ac1) => elm._1 == ac1._2).isEmpty)) ((row._2,elm._2),elm._1) :: acc
+                else if (acc.filter((ac1) => c(ac1._2,elm._1)).isEmpty) List(((row._2,elm._2),elm._1))
+                else acc
+            })
+    
+        def maxes : List[Element] = data.zipWithIndex.flatMap(elmInRow(_, (_ > _)));
+        
+        // first transpose the matrix, then swap back rows with columns
+        def mins : List[Element] = data.transpose.zipWithIndex.flatMap(elmInRow(_, (_ < _))).map( (elm : Element) => (elm._1.swap,elm._2));
+
+        mins.filter(maxes.contains(_)).map(_._1).toSet
+
+    }
+
+}
+
+object Matrix {
+    def apply(data : List[List[Int]]) = new Matrix(data) 
+}
