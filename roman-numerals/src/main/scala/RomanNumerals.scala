87208d31d1281a90d6cdb741b745a2be04045b83
Brambilla <Alessandro.Brambilla@acer.com>
2019-05-17 15:54:23 +0200

    Better Roman Numerals

--- roman-numerals/src/main/scala/RomanNumerals.scala
+++ roman-numerals/src/main/scala/RomanNumerals.scala
@@ -1,17 +1,14 @@
 
 object RomanNumerals{
 
-    val dict : List[(Int,String)] = List((1000,"M"),(900, "CM"),(500,"D"),(400, "CD"),(100,"C"),(90,"XC"),
+    val dicts : List[(Int,String)] = List((1000,"M"),(900, "CM"),(500,"D"),(400, "CD"),(100,"C"),(90,"XC"),
     (50,"L"),(40,"XL"),(10,"X"),(9,"IX"),(5,"V"),(4,"IV"),(1,"I"))
 
     def roman(num : Int) : String = {
-        def toRoman(n : Int, ds: List[(Int,String)]) : String = ds match {
-            case Nil => ""
-            case _ => {
-                val d = ds.head;
-                d._2 * (n / d._1) + toRoman(n % d._1, ds.tail) 
-            }
-        }
-        toRoman(num, dict)
+        dicts.foldLeft("",num)((acc,dict) => {
+            val elm = dict._2 * (acc._2 / dict._1)
+            (acc._1 + elm, acc._2 % dict._1)
+         }
+        )._1        
     }
 }
